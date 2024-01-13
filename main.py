a65425ba7c0aa7a0d198d08d32525d1c3d24dc4a
Sahil Shah <shahsahils100@gmail.com>
2024-01-13 15:30:06 +0530

    Calculated similarity matrix succesfully

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import numpy as np
 import pandas as pd
 import math
 
-data = pd.read_csv("newdataset_spectral_clustering.dat", delimiter="\s+")
+DATA = pd.read_csv("newDATAset_spectral_clustering.dat", delimiter="\s+")
 
-data.columns = data.columns.str.strip()
-data.replace([np.inf, -np.inf], np.nan, inplace=True)
-data.dropna(inplace=True)
+DATA.columns = DATA.columns.str.strip()
+DATA.replace([np.inf, -np.inf], np.nan, inplace=True)
+DATA.dropna(inplace=True)
 
-data = data.dropna()
+DATA = DATA.dropna()
 
-data.reset_index(inplace=True)
+DATA.reset_index(inplace=True)
 
 selected_columns = [
     "AOD440",
@@ -39,84 +39,118 @@ selected_columns = [
     "FMF500",
 ]
 
-data = data[selected_columns]
+DATA = DATA[selected_columns]
 
 
-def calculate_mahalanobis(data):
-    N = len(data.axes[0])
-    P = len(data.axes[1])
+N = len(DATA.axes[0])
+P = len(DATA.axes[1])
 
-    mean = data.mean(axis=0)
+MEAN = DATA.mean(axis=0)
 
-    standard_deviation = data.std()
+STANDARD_DEVIATION = DATA.std()
 
-    variables = list(data.columns)
+VARIABLES = list(DATA.columns)
 
+
+def calculate_cov_matrix_corr_matrix():
     cov_matrix = np.zeros((P, P), dtype=np.float64)
 
     corr_matrix = np.zeros((P, P), dtype=np.float64)
 
     for i in range(0, P):
-        pi = variables[i]
-        for j in range(0, P):
-            pj = variables[j]
-            if i >= j:
-                for k in range(0, N):
-                    cov_matrix[i][j] += (data[pi][k] - mean[pi]) * (
-                        data[pj][k] - mean[pi]
-                    )
-                cov_matrix[i][j] = cov_matrix[i][j] / (N - 1)
-                corr_matrix[i][j] = cov_matrix[i][j] / (
-                    standard_deviation[pi] * standard_deviation[pj]
-                )
-            else:
-                continue
+        pi = VARIABLES[i]
+        for j in range(i, P):
+            pj = VARIABLES[j]
+            for k in range(0, N):
+                cov_matrix[i][j] += (DATA[pi][k] - MEAN[pi]) * (DATA[pj][k] - MEAN[pi])
+            cov_matrix[i][j] = cov_matrix[i][j] / (N - 1)
+            corr_matrix[i][j] = cov_matrix[i][j] / (
+                STANDARD_DEVIATION[pi] * STANDARD_DEVIATION[pj]
+            )
 
     for i in range(0, P):
-        for j in range(0, P):
-            if i < j:
-                cov_matrix[i][j] = cov_matrix[j][i]
-                corr_matrix[i][j] = corr_matrix[j][i]
+        for j in range(0, i):
+            cov_matrix[i][j] = cov_matrix[j][i]
+            corr_matrix[i][j] = corr_matrix[j][i]
 
+    return cov_matrix, corr_matrix
+
+
+def calculate_WMD(corr_matrix):
     WMD = np.zeros((N, N), dtype=np.float64)
 
-    X = np.array(data.loc[3])
-    Y = np.array(data.loc[5])
+    for i in range(0, N):
+        for j in range(i, N):
+            X = np.array(DATA.loc[i])
+            Y = np.array(DATA.loc[j])
 
-    X = np.transpose(X)
-    Y = np.transpose(Y)
+            X = np.transpose(X)
+            Y = np.transpose(Y)
 
-    result = np.dot(corr_matrix, X - Y)
+            ans = np.dot(corr_matrix, X - Y)
+            ans = np.dot(np.transpose(X - Y), ans)
+            ans = math.sqrt(ans)
 
-    result = np.dot(np.transpose(X - Y), result)
-
-    print(result)
+            WMD[i][j] = ans
 
     for i in range(0, N):
-        for j in range(0, N):
-            if i >= j:
-                X = np.array(data.loc[i])
-                Y = np.array(data.loc[j])
-
-                X = np.transpose(X)
-                Y = np.transpose(Y)
-
-                ans = np.dot(corr_matrix, X - Y)
-                ans = np.dot(np.transpose(X - Y), ans)
-                ans = math.sqrt(ans)
-
-                WMD[i][j] = ans
-
-            else:
-                continue
-
-    for i in range(0, N):
-        for j in range(0, N):
-            if i < j:
-                WMD[i][j] = WMD[j][i]
+        for j in range(0, i):
+            WMD[i][j] = WMD[j][i]
 
     return WMD
 
 
-# print(np.array(data.loc[[1]]))
-calculate_mahalanobis(data)
+def calculate_similarity_matrix(WMD, k=3, z=3):
+    k_nn = np.zeros((N, N), dtype=np.bool_)
+
+    for i in range(0, N):
+        neighbor_WMD = WMD[i].copy()
+        neighbor_WMD[i] = math.inf
+
+        for j in range(0, k):
+            min_WMD = min(neighbor_WMD)
+
+            idx_neighbor = np.where(neighbor_WMD == min_WMD)[0][0]
+            neighbor_WMD[idx_neighbor] = math.inf
+
+            k_nn[i][idx_neighbor] = 1
+
+    knn_data = pd.DataFrame(k_nn)
+    knn_data.to_csv("k_nn_datafile.csv")
+
+    # for i in range(0, N):
+    #     for j in range(i + 1, N):
+    #         if k_nn[i][j] and k_nn[j][i]:
+    #             print((i, j))
+
+    similarity_matrix = np.zeros((N, N), dtype=np.float64)
+
+    for i in range(0, N):
+        for j in range(i, N):
+            if k_nn[i][j] and k_nn[j][i]:
+                coeff = (WMD[i][j] ** 2) / (2 * z)
+                similarity_matrix[i][j] = math.exp(-coeff)
+                similarity_matrix[j][i] = similarity_matrix[i][j]
+
+    similarity_matrix_data = pd.DataFrame(similarity_matrix)
+
+    similarity_matrix_data.to_csv("similarity_matrix_data.csv")
+
+
+matrices = calculate_cov_matrix_corr_matrix()
+
+print("Calculated T and R")
+
+cov_matrix_T = matrices[0]
+corr_matrix_R = matrices[1]
+
+WMD_matrix = calculate_WMD(corr_matrix_R)
+
+print("Calculated WMD")
+
+calculate_similarity_matrix(WMD_matrix, 3)
+
+print("Done")
+
+
+# print(np.array(DATA.loc[[1]]))
